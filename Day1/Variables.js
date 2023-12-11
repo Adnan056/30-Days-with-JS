79420998da644d1724b5d353814195d80ab1e377
Adnan056 <adnansohail1@yahoo.com>
2023-12-11 13:51:20 +0530

    v1

---
+++ Day1/Variables.js
@@ -0,0 +1,8 @@
+let firstname="Adnan"
+let lastname="Soahil"
+let country="India"
+let city="Hyderabad"
+let gender='M';age=22;isMarried=false;
+let cgpa=6.8;inter=671;hs='74%';
+const boilingPoint=100;gravity=9.81;PI=3.14;
+let name="Adnan";job="unemployed";live="India"
---
+++ Day1/main.js
@@ -0,0 +1,3 @@
+console.log(firstname,lastname,country,city,gender,isMarried,age)
+console.log(cgpa,inter,hs,gravity,boilingPoint,PI)
+console.log(name,job,live)
